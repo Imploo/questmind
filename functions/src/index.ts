6d5361f54c77471291d4985dbcb017a8e5ce12e5
MattArcady <m.bultje@arcady.nl>
2026-02-03 09:41:08 +0000

    build fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,5 @@
 import { setGlobalOptions } from 'firebase-functions/v2';
 import { initializeApp } from 'firebase-admin/app';
-import ffmpeg from 'fluent-ffmpeg';
-import ffmpegInstaller from '@ffmpeg-installer/ffmpeg';
 
 setGlobalOptions({
   region: 'europe-west4',
@@ -10,6 +8,5 @@ setGlobalOptions({
 });
 
 initializeApp();
-ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 export { generatePodcastAudio } from './generate-podcast-audio';
